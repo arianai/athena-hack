14e733871d31d84170758c49a7cfd64bfc62f388
Abi Wilson <abigail.wilson@dazn.com>
2022-06-11 20:22:26 +0100

    feat: add boilerplate index and server files

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const PORT = process.env.PORT || 3000;
+
+app.get("^/$|/index(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
