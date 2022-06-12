ac3e76ad5a198d9fff32e4c3c1fc20015c0687b2
Abi Wilson <abigail.wilson@dazn.com>
2022-06-12 10:41:07 +0100

    style: add styling for index

--- server.js
+++ server.js
@@ -3,16 +3,6 @@ const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3000;
 
-app.get("^/$|/index(.html)?", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-app.get("^/$|/home(.html)?", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "home.html"));
-});
-
-app.get("^/$|/meeting(.html)?", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "meeting.html"));
-});
+app.use(express.static("public"));
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
